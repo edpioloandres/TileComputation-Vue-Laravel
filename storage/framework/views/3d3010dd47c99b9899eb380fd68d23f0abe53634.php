4509fbb38828a4a6da5b08809783060fb6e95d93
andrs_p <125113010+edpioloandres@users.noreply.github.com>
2023-03-20 16:31:56 +0800

    Add files via upload

---
+++ storage/framework/views/3d3010dd47c99b9899eb380fd68d23f0abe53634.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bankin Estimations</title>
+    <link rel="shortcut icon" href="./bankinlogo.ico">
+</head>
+<body>
+    <div id="app"></div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+</body>
+</html><?php /**PATH C:\Users\smd154\Desktop\New folder\BANKIN 070821\BankinEstimation\resources\views/layouts/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e9d436c0efbc843f4526c433be8e4194dcf0e962.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gaiheki Tile Computation</title>
+</head>
+<body >
+
+    <div id="app"></div>
+    <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+
+</body>
+</html><?php /**PATH C:\Users\waku129\Desktop\solar_estimations\resources\views/layouts/app.blade.php ENDPATH**/ ?>
